6bd0b200c84ebfcdec102cd2e2c8420597894699
Kuransatsu <144381478+Kuransatsu@users.noreply.github.com>
2024-08-02 02:04:00 +0300

    3

--- Vampy's Delivery Services/Assets/player-code/Shooting codes/Trident.cs
+++ Vampy's Delivery Services/Assets/player-code/Shooting codes/Trident.cs
@@ -15,22 +15,25 @@ public class Trident : MonoBehaviour
     {
         Camera = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
         AimTransform = GameObject.Find("Aim").GetComponentInParent<Transform>();
+        MousePos = Camera.ScreenToWorldPoint(Input.mousePosition);
+        LookDir = (MousePos - AimTransform.position);
+        print(LookDir.x);
     }
 
     // Update is called once per frame
     void Update()
     {
-        MousePos = Camera.ScreenToWorldPoint(Input.mousePosition);
-        LookDir = (MousePos - AimTransform.position);
-        print(LookDir.x);
+        
         if (LookDir.x > 0)
         {
+            print('1');
             Quaternion target = Quaternion.Euler(0, 0, -90);
             transform.rotation = Quaternion.Slerp(transform.rotation, target, 0.8f*Time.deltaTime);
         }
-        else
+        if (LookDir.x < 0) 
         {
-            Quaternion target = Quaternion.Euler(0, 0, 90);
+            print('2');
+            Quaternion target = Quaternion.Euler(0, 0, 270);
             transform.rotation = Quaternion.Slerp(transform.rotation, target, 0.8f*Time.deltaTime);
         }
      
